ce5af00749f1fff8b8120980f5852651c49b4deb
shugo1125 <iamsyu25@icloud.com>
2024-09-30 14:40:32 +0900

    no message

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -14,18 +14,26 @@ class PostController extends Controller
     $posts = DB::table('posts')->get();
     return view('posts.index', compact('posts'));
   }
-
-
   public function show($id)
   {
     // URL'/products/{id}'の'{id}'部分と主キー（idカラム）の値が一致するデータをproductsテーブルから取得し、変数$productに代入する
     $post = Post::find($id);
-
-
-
-
-
     // 変数$productをproducts/show.blade.phpファイルに渡す
     return view('posts.show', compact('post'));
+  }
+  public function create()
+  {
+    return view('posts.create');
+  }
+
+  public function store(Request $request)
+  {
+    // バリデーションを設定する
+    $request->validate([
+      'title' => 'required|max:20',
+      'content' => 'required|max:200'
+    ]);
+
+    return redirect('/posts');
   }
 }
